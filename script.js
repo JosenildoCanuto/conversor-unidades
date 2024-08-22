a4431628826daa5fccc64ac4e30fcc1046b467d4
JosenildoCanuto <josenildo.acanuto@gmail.com>
2024-08-21 23:56:35 -0300

    logic-js-one

--- script.js
+++ script.js
@@ -1,49 +1,43 @@
 let inputUnidades = document.querySelector('.select-options')
-let selectsOne = document.querySelector('.select-unidades-one')
-let selectsTwo = document.querySelector('.select-unidades-two')
+let origemInput = document.querySelector('.select-unidades-one')
+let destinoInput = document.querySelector('.select-unidades-two')
 let inputValue = document.querySelector('.valor-input')
+let buttonConvert = document.querySelector('.button-convert')
 
 
 let units = {
-    comprimento: ["Quilômetro (km)", "Hectômetro (hm)", "Decâmetro (dam)", "Milímetro (mm)", "Centímetro (cm)", "Decímetro (dm)"],
+    comprimento: ["Metro (m)","Quilômetro (km)", "Milímetro (mm)",],
     temperatura: ["Celsius (°C)", "Graus Fahrenheit (°F)","Kelvin (K)"],
-    peso: ["Decagrama (dag)","Hectograma (hg)", "Quilograma (kg)", "Decigrama (dg)", "Centigrama (cg)", "Miligrama (mg)"]
+    peso: ["Tonelada (t)", "Quilograma (kg)", "Grama (g)"]
 }
 
 function uptadeSelectOptions (){
     let selectUnits = units[inputUnidades.value] || [];
 
-        selectsOne.innerHTML = '';
+        origemInput.innerHTML = '';
 
         selectUnits.forEach(unit => {
             let newOption = document.createElement('option');
             newOption.textContent = unit;
-            selectsOne.appendChild(newOption);
+            origemInput.appendChild(newOption);
         })
 
-        selectsTwo.innerHTML = '';
+        destinoInput.innerHTML = '';
 
         selectUnits.forEach(unit => {
             let newOption = document.createElement('option');
             newOption.textContent = unit;
-            selectsTwo.appendChild(newOption);
+            destinoInput.appendChild(newOption);
         })
 }
 
-selectsOne.addEventListener('change', function() {
-    selectedIndexOne = parseInt(selectsOne.value, 10);
-    calcComprimento();
-});
 
-selectsTwo.addEventListener('change', function() {
-    selectedIndexTwo = parseInt(selectsTwo.value, 10);
-    calcComprimento();
-});
-
-function calcComprimento (){
-    if(selectedIndexOne > selectedIndexTwo){
-        console.log(inputValue.value * Math.pow(10, selectedIndexTwo));
+function calcComprimento(valor, valorOrigem, ValorDestino){
+    if(valorOrigem === 'm' || valorOrigem === 'Metro (m)' && ValorDestino === 'km' || ValorDestino === 'Quilômetro (Km)'){
+        console.log(valor * 100);
     }
 }
 
-uptadeSelectOptions();
+
+console.log(calcComprimento(inputValue.value, origemInput.value, destinoInput.value))
+inputUnidades.addEventListener('change', uptadeSelectOptions);
