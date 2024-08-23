e9096b849d77ed1b07e014b787a78164693b7434
JosenildoCanuto <josenildo.acanuto@gmail.com>
2024-08-23 15:11:30 -0300

    medidas-calculos

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-let inputUnidades = document.querySelector('.select-options')
-let origemInput = document.querySelector('.select-unidades-one')
-let destinoInput = document.querySelector('.select-unidades-two')
-let inputValue = document.querySelector('.valor-input')
-let buttonConvert = document.querySelector('.button-convert')
-
+let inputUnidades = document.querySelector('.select-options');
+let origemInput = document.querySelector('.select-unidades-one');
+let destinoInput = document.querySelector('.select-unidades-two');
+let inputValue = document.querySelector('.valor-input');
+let buttonConvert = document.querySelector('.button-convert');
+let result = document.querySelector('.result');
 
 let units = {
-    comprimento: ["Metro (m)","Quilômetro (km)", "Milímetro (mm)",],
+    comprimento: ["Quilômetro (km)", "Metro (m)", "Centímetros (cm)",],
     temperatura: ["Celsius (°C)", "Graus Fahrenheit (°F)","Kelvin (K)"],
     peso: ["Tonelada (t)", "Quilograma (kg)", "Grama (g)"]
 }
+
+let valueUnits = {
+    comprimento: ["km", "m", "cm"],
+    temperatura: ["°C", "°F","K"],
+    peso: ["t", "kg", "g"]
+}
+
+uptadeSelectOptions()
 
 function uptadeSelectOptions (){
     let selectUnits = units[inputUnidades.value] || [];
 
         origemInput.innerHTML = '';
 
-        selectUnits.forEach(unit => {
+        selectUnits.forEach((unit, i) => {
             let newOption = document.createElement('option');
             newOption.textContent = unit;
             origemInput.appendChild(newOption);
+            console.log(i)
         })
 
         destinoInput.innerHTML = '';
@@ -32,12 +41,85 @@ function uptadeSelectOptions (){
 }
 
 
-function calcComprimento(valor, valorOrigem, ValorDestino){
-    if(valorOrigem === 'm' || valorOrigem === 'Metro (m)' && ValorDestino === 'km' || ValorDestino === 'Quilômetro (Km)'){
-        console.log(valor * 100);
+function calcComprimento(valor, valorOrigem, valorDestino){
+    if(valorOrigem === valorDestino){
+        return valor
+    }
+
+    if (valorOrigem === 'Quilômetro (Km)' && valorDestino === 'Metro (m)'){
+        return valor*1000;
+    } else if (valorOrigem === 'Quilômetro (Km)' && valorDestino === 'Centímetros (cm)'){
+        return valor*100000;
+    } else if (valorOrigem === 'Metro (m)' && valorDestino === 'Quilômetro (Km)'){
+        return valor / 1000;
+    } else if (valorOrigem === 'Metro (m)' && valorDestino === "Centímetros (cm)"){
+        return valor * 100;
+    } else if (valorOrigem === 'Centímetros (cm)' && valorDestino === 'Quilômetro (Km)'){
+        return valor / 100000;
+    } else if (valorOrigem === 'Centímetros (cm)' && valorDestino === 'Metro (m)'){
+        return valor / 100;
+    } 
+}
+
+function calcTemperatura(valor, valorOrigem, valorDestino) {
+    if((valorOrigem === 'Celsius (°C)') && (valorDestino === 'Graus Fahrenheit (°F)')){
+        return valor * 1.8 + 32;
+    } else if ((valorOrigem === 'Celsius (°C)') && (valorDestino === 'Kelvin (K)')){
+        return valor + 273.15;
+    } else if((valorOrigem === 'Graus Fahrenheit (°F)') && (valorDestino === 'Celsius (°C)')){
+        return (valor - 32) / 1.8;
+    } else if ((valorOrigem === 'Graus Fahrenheit (°F)') && (valorDestino === 'Kelvin (K)')){
+        return (valor - 32) * 5 / 9 + 273.15;
+    } else if((valorOrigem === 'Kelvin (K)') && (valorDestino === 'Celsius (°C)')){
+        return valor - 273.15;
+    } else if ((valorOrigem === 'Kelvin (K)') && (valorDestino === 'Graus Fahrenheit (°F)')){
+        return (valor - 273.15) * 1.8 + 32;
+    } else {
+        return valor;
     }
 }
 
+function calcMassa(valor, valorOrigem, valorDestino) {
+    if((valorOrigem === 'Tonelada (t)') && (valorDestino === 'Quilograma (kg)')){
+        return valor * 1000;
+    } else if ((valorOrigem === 'Tonelada (t)') && (valorDestino === 'Grama (g)')){
+        return valor * 1000000;
+    } else if((valorOrigem === 'Quilograma (kg)') && (valorDestino === 'Tonelada (t)')){
+        return valor / 1000;
+    } else if ((valorOrigem === 'Quilograma (kg)') && (valorDestino === 'Grama (g)')){
+        return valor * 1000;
+    } else if((valorOrigem === 'Grama (g)') && (valorDestino === 'Tonelada (t)')){
+        return valor * 1000000;
+    } else if ((valorOrigem === 'Grama (g)') && (valorDestino === 'Quilograma (kg)')){
+        return valor / 1000;
+    } else {
+        return valor;
+    }
+}
 
-console.log(calcComprimento(inputValue.value, origemInput.value, destinoInput.value))
+function calcular(){
+    result.innerHTML = '';
+
+    let valor = parseFloat(inputValue.value);
+
+    if(inputValue.value === ''){
+        result.innerHTML = 'Digite algum valor'
+    }
+
+    let origem = origemInput.value;
+    let destino = destinoInput.value;
+    
+    if(inputUnidades.value === 'comprimento'){
+        resultValor = (calcComprimento(valor, origem, destino));
+    } else if(inputUnidades.value === 'temperatura'){
+        resultValor = (calcTemperatura(valor, origem, destino));
+    } else {
+        resultValor = (calcMassa(valor, origem, destino));
+    }
+
+    result.innerHTML = resultValor+ ' ' + destino;
+
+}
+
+buttonConvert.addEventListener('click', calcular)
 inputUnidades.addEventListener('change', uptadeSelectOptions);
